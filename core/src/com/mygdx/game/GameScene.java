3fbe1b660296e3f92a2f0629e0aff80fd43c2359
wietze <wietze.pc@gmail.com>
2017-03-24 00:35:07 +0100

    fixed gameplay  so that the game is not immposible

--- core/src/com/mygdx/game/GameScene.java
+++ core/src/com/mygdx/game/GameScene.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.CatmullRomSpline;
@@ -20,7 +23,7 @@ import java.util.Iterator;
 public class GameScene extends BaseScene {
 
     boolean canAddBall = true;
-    float timeOneSpin = 6.7f;
+
 
     SpriteBatch batch;
     Sprite background;
@@ -28,8 +31,8 @@ public class GameScene extends BaseScene {
     CatmullRomSpline<Vector2> myCatmull;
 
     float score = 0;
-    float spawnSpeedMin = 2f;
-    float spawnSpeedMax = 4.5f;
+    float spawnSpeedMin = 3f;
+    float spawnSpeedMax = 5f;
 
     float minTime = 0f;
     float wallspeed = 4;
@@ -38,12 +41,23 @@ public class GameScene extends BaseScene {
     ArrayList<Wall> walls;
 
     float counter = 0;
+    float scoreCounter = 0;
     Loop game;
+    int spawnFreq = 0;
+    int maxBalls = 3;
+    int maxSpeed = 20;
 
+    BitmapFont fontScore;
+    GlyphLayout layout;
+    float textWidthScore;
+    int scoreLim = 0;
+    int minScoreLim = 0;
+    int maxScoreLim = 1;
 
-    public GameScene(Loop g){
+    public GameScene(Loop g) {
         super(g);
         game = g;
+
     }
 
     @Override
@@ -57,32 +71,36 @@ public class GameScene extends BaseScene {
         touchPos = new Vector3();
 
         background = new Sprite(new Texture("infin.jpg"));
-        Ball ball = new Ball(myCatmull);
         balls = new ArrayList<Ball>();
-        balls.add(ball);
 
         walls = new ArrayList<Wall>();
+
+        fontScore = new BitmapFont(Gdx.files.internal("scoreFont3.fnt"));
+        fontScore.setColor(Color.BLACK);
+        fontScore.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        layout = new GlyphLayout(fontScore, "Score 0");
+        textWidthScore = layout.width * 0.5f;
+
     }
 
-    public void setup(){
+    public void setup() {
         int k = 500; //increase k for more fidelity to the spline
         Vector2[] points = new Vector2[k];
         Vector2 cp[] = new Vector2[]{
-                new Vector2(144,480), new Vector2(145,450), new Vector2(150,419), new Vector2(157,381), new Vector2(177,325), new Vector2(197,291), new Vector2(212,272),
-                new Vector2(237,248), new Vector2(285,218), new Vector2(351,210), new Vector2(392,219), new Vector2(443,248), new Vector2(499,300), new Vector2(547,355),
-                new Vector2(579,395), new Vector2(625,462), new Vector2(677,536), new Vector2(732,607), new Vector2(777,659), new Vector2(810,692), new Vector2(861,729),
-                new Vector2(909,748), new Vector2(963,750), new Vector2(1024,725), new Vector2(1081,667), new Vector2(1109,616), new Vector2(1124,568), new Vector2(1132,514),
-                new Vector2(1132,441), new Vector2(1118,376), new Vector2(1092,312), new Vector2(1056,261), new Vector2(1000,220), new Vector2(956,210), new Vector2(890,218),
-                new Vector2(808,273), new Vector2(733,353), new Vector2(668,442), new Vector2(591,548), new Vector2(517,641), new Vector2(428,723), new Vector2(358,751),
-                new Vector2(274,737), new Vector2(223,700), new Vector2(185,652), new Vector2(164,599), new Vector2(148,535)
+                new Vector2(144, 480), new Vector2(145, 450), new Vector2(150, 419), new Vector2(157, 381), new Vector2(177, 325), new Vector2(197, 291), new Vector2(212, 272),
+                new Vector2(237, 248), new Vector2(285, 218), new Vector2(351, 210), new Vector2(392, 219), new Vector2(443, 248), new Vector2(499, 300), new Vector2(547, 355),
+                new Vector2(579, 395), new Vector2(625, 462), new Vector2(677, 536), new Vector2(732, 607), new Vector2(777, 659), new Vector2(810, 692), new Vector2(861, 729),
+                new Vector2(909, 748), new Vector2(963, 750), new Vector2(1024, 725), new Vector2(1081, 667), new Vector2(1109, 616), new Vector2(1124, 568), new Vector2(1132, 514),
+                new Vector2(1132, 441), new Vector2(1118, 376), new Vector2(1092, 312), new Vector2(1056, 261), new Vector2(1000, 220), new Vector2(956, 210), new Vector2(890, 218),
+                new Vector2(808, 273), new Vector2(733, 353), new Vector2(668, 442), new Vector2(591, 548), new Vector2(517, 641), new Vector2(428, 723), new Vector2(358, 751),
+                new Vector2(274, 737), new Vector2(223, 700), new Vector2(185, 652), new Vector2(164, 599), new Vector2(148, 535)
 
         };
 
-        myCatmull = new CatmullRomSpline<Vector2>( cp, true);
-        for(int i = 0; i < k; ++i)
-        {
+        myCatmull = new CatmullRomSpline<Vector2>(cp, true);
+        for (int i = 0; i < k; ++i) {
             points[i] = new Vector2();
-            myCatmull.valueAt(points[i], ((float)i)/((float)k-1));
+            myCatmull.valueAt(points[i], ((float) i) / ((float) k - 1));
         }
     }
 
@@ -95,55 +113,57 @@ public class GameScene extends BaseScene {
         background.draw(batch);
         Iterator<Ball> iter = balls.iterator();
 
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             Ball b = iter.next();
             b.draw(batch);
         }
         Iterator<Wall> wallIter = walls.iterator();
 
-        while(wallIter.hasNext()){
+        while (wallIter.hasNext()) {
             Wall w = wallIter.next();
             w.draw(batch);
 
 
         }
+
+        fontScore.draw(batch, "Score: " + (int)score, game.screenWidth *0.8f - (textWidthScore / 2), game.screenHeight * 0.9f);
         batch.end();
 
         update(delta);
     }
 
-    public void update(float delta){
-        if(Gdx.input.isTouched()){
-            touchPos.set(Gdx.input.getX(),Gdx.input.getY(),0);
+    public void update(float delta) {
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             game.camera.unproject(touchPos);
 
-            if(touchPos.x>game.screenWidth/2){
-                spawnSpeed = MathUtils.random(spawnSpeedMin,spawnSpeedMax) * 0.5f;
+            if (touchPos.x > game.screenWidth / 2) {
+                spawnSpeed = MathUtils.random(spawnSpeedMin, spawnSpeedMax) * 0.5f;
                 Iterator<Wall> iter = walls.iterator();
 
-                while(iter.hasNext()){
+                while (iter.hasNext()) {
                     Wall w = iter.next();
-                    w.setSpeed(wallspeed *1.8f);
+                    w.setSpeed(wallspeed * 1.8f);
                     w.update();
 
                 }
-            }else{
-                spawnSpeed = MathUtils.random(spawnSpeedMin,spawnSpeedMax) * 2f;
+            } else {
+                spawnSpeed = MathUtils.random(spawnSpeedMin, spawnSpeedMax) * 2f;
                 Iterator<Wall> iter = walls.iterator();
 
-                while(iter.hasNext()){
+                while (iter.hasNext()) {
                     Wall w = iter.next();
-                    w.setSpeed(wallspeed *0.5f);
+                    w.setSpeed(wallspeed * 0.25f);
                     w.update();
-
                 }
+
             }
 
-        }else {
-            spawnSpeed = MathUtils.random(spawnSpeedMin,spawnSpeedMax);
+        } else {
+            spawnSpeed = MathUtils.random(spawnSpeedMin, spawnSpeedMax);
             Iterator<Wall> iter = walls.iterator();
 
-            while(iter.hasNext()){
+            while (iter.hasNext()) {
                 Wall w = iter.next();
                 w.setSpeed(wallspeed);
                 w.update();
@@ -151,52 +171,55 @@ public class GameScene extends BaseScene {
         }
 
         counter += delta;
-        if(counter > spawnSpeed +minTime){ //TODO Remove wall if outside screen
+        if (counter > spawnSpeed + minTime) {
             walls.add(new Wall(game));
             counter = 0;
         }
 
-        System.out.println(balls.get(0).getPos());
-        System.out.println(counter);
-        Iterator<Ball> iter = balls.iterator();
+        Iterator<Wall> wallitter = walls.iterator();
+        while (wallitter.hasNext()) {
+            Wall w = wallitter.next();
+            if (w.wall.getY() > 1000) {
+                walls.remove(w);
+            }
+        }
+        scoreCounter = scoreCounter + delta;
+        if(scoreCounter > 0.11f){
+            score++;
+            scoreCounter = 0;
+            canAddBall = true;
+        }
 
-        while(iter.hasNext()){
-            Ball b = iter.next();
-            if(b.getPos().x<(game.screenWidth /2) +3 && b.getPos().x > (game.screenWidth/2) -3&& b.canScore){
-                score++;
-                b.canScore = false;
-            }else {
-                b.canScore = true;
+        if (score % 12 == 0) {
+            Iterator<Ball> Biter = balls.iterator();
+            while (Biter.hasNext()) {
+                Ball b = Biter.next();
+                if(!(b.speed >= maxSpeed)){
+                    b.speed+=0.2f;
+                }
+
+
             }
         }
 
-
-        if(score == 1){
-            if(canAddBall){
-                balls.add(new Ball(myCatmull));
-                canAddBall = false;
-            }
+        if (score > scoreLim && canAddBall && balls.size() < maxBalls){
+            Ball b = new Ball(myCatmull);
+            balls.add(b);
+            canAddBall = false;
+            scoreLim += MathUtils.random(minScoreLim+200,maxScoreLim + 500);
         }
-        if(score >1 && score <3)
-            canAddBall=true;
+        System.out.println(score);
+    }
+        @Override
+        public void resize ( int width, int height){
+            super.resize(width, height);
 
-        if(score == 3){
-            if(canAddBall){
-                balls.add(new Ball(myCatmull));
-                canAddBall = false;
-            }
         }
 
-    }
-    @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
-
-    }
-
-    @Override
-    protected void handleBackPress() {
-        super.handleBackPress();
-    }
+        @Override
+        protected void handleBackPress () {
+            super.handleBackPress();
+        }
 }
+
 
